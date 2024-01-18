b1f3430c8df52bc7d9a7c78a95e6a813d4edfa47
returnaction <obergannv@gmail.com>
2024-01-18 13:40:34 -0500

    Add project files.

---
+++ Delete/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Delete.Models;
+
+namespace Delete.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
